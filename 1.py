2bc9c5638d949665468ab450ec885835510065fd
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-08-02 23:35:43 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,2 +1,24 @@
-#1st Python Code
-print("Welcome to my Github account this is my first Python Code")
+"""
+Task:
+You have to create a program where you have to create a list of first 5 and last 5 numbers in range of number 1 to 30.
+
+______________________________________________________________________________________"""
+
+List= [ ]
+i = 0
+j= 0
+a = 1#int(input("Enter First Number:- "))
+b = 30#int(input("Enter First Number:- "))
+
+List.append(a)
+for i in range(4):
+	a=a+1
+	#print(a)
+	List.append(a)
+List.append(b)
+for j in range(4):
+	b=b-1
+	#print(b)
+	List.append(b)
+
+print(List)		
