a404aa388233b808f1647ad3e2c299347d788dd6
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-08-15 19:57:13 +0530

    Add files via upload

---
+++ Integer_Form.py
@@ -0,0 +1,9 @@
+#Create a python program that can show that given number is positive ,negativeor zero
+
+n = int(input("Enter Number: "))
+if n== 0 :
+	print("Given input is Zero")
+elif n>0:
+	print("Given input is Positive")
+else:
+	print("Given input is Negative")
