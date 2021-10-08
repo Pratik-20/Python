f6c7edfa30866e9a0f4f9f4c18d2de91a8dce038
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2021-10-08 14:37:31 +0530

    Add files via upload

---
+++ 1_n.cxx
@@ -0,0 +1,16 @@
+
+//Print numbers from 1 to n without using a semicolon?
+
+
+//Print numbers from 1 to n without using a semicolon?
+
+#include<iostream>
+int main()
+{
+	int i,n;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)	    
+	      //printf("%d",i);           //One Line
+	      printf("%d\n",i);         //New Line
+	return 0;
+}
