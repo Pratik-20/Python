3dc16a59305a3a0316f59758dd7182720754d12c
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-01-20 16:50:35 +0530

    Create String_Slice.py

---
+++ String_Slice.py
@@ -0,0 +1,32 @@
+# Python program to demonstrate 
+
+# string slicing 
+
+  
+
+# String slicing  
+
+String ='ASTRING'
+
+  
+
+# Using slice constructor 
+
+s1 = slice(3) 
+
+s2 = slice(1, 5, 2)  
+
+s3 = slice(-1, -12, -2) 
+
+  
+
+print("String slicing")  
+
+print(String[s1])  
+
+print(String[s2])  
+
+print(String[s3]) 
+//Reverse Line Print
+
+print(String[::-1])
