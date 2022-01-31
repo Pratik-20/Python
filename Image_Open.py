f162cd883e05b39921f50f27e0a9f7952d0b9919
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-01-31 23:32:50 +0530

    Add files via upload

---
+++ Image_Open.py
@@ -0,0 +1,12 @@
+# Imports PIL module  
+
+from PIL import Image 
+
+  
+# open method used to open different extension image file 
+
+im = Image.open(r"/storage/emulated/0/CV/Train/metal/0.jpg")  
+
+  
+# This method will show image in any image viewer  
+im.show()  
