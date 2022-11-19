683e912c5bc6afad37de89e3291dfb944ab9594b
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-11-19 19:19:46 +0530

    Add files via upload

---
+++ Savings account .py
@@ -0,0 +1,21 @@
+"""
+Program with numbers
+
+Savings account
+
+For the given amount of money, calculate the income from this savings account with a 5% interest rate after one year.
+
+Here's the formula for calculating the income: income = \frac {amount \times interest \times years}{100}
+income=  amount×interest×years/100
+Save the result into the variable income. You DO NOT need to print it.
+
+
+"""
+
+
+
+amount = 1000
+interest_rate = 5
+years = 1
+# change the next line
+income = (amount * interest_rate * years) / 100
