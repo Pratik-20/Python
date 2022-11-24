b02837a67cf0b2d0622ddd972e3dc7d5697350f6
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-11-25 00:15:28 +0530

    Add files via upload

---
+++ Empty the pencil case .py
@@ -0,0 +1,29 @@
+"""
+Work on project. Stage 1/5: Empty the pencil case
+Project: Last Pencil
+
+Empty the pencil case
+
+Description
+You and your friend decided to play a simple pen-and-paper game.
+There's one catch — you have only pencils but no paper; the last piece of paper is gone for your beautiful drawings.
+As a joke, your friend pulls all the pencils out of the case onto the table and says: "Your turn!"
+
+Objectives In this stage, your program should print two lines: one line with several vertical bar symbols
+representing pencils (for example, ||| or |||||||) and one Your turn! string.
+
+Examples
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+
+Example 1:
+
+||||||||
+Your turn!
+Example 2:
+
+|||
+Your turn!
+"""
+
+print("||")
+print("Your turn!")
