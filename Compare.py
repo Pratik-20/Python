f7d404ce43d0e3d4882f77b466ac979ea38ae3c8
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2022-12-10 23:13:35 +0530

    Add files via upload

---
+++ Compare.py
@@ -0,0 +1,12 @@
+first = input()
+second = input()
+
+first = int(first)
+second = int(second)
+
+if first > second:
+    print("The first one wins")
+elif second > first:
+    print("The second one wins")
+else:
+    print("Draw")
